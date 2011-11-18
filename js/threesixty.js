669b81b661f62735ab3ab57cfa7c0f675d0549eb
Robert Pataki <heartcode@robertpataki.com>
2011-11-18 20:57:17 +0000

    Minor changes
    - JS minor changes, optimised code; Loading percentage added. Preloader style changed.
    - Loading span added, preloaded style changed in CSS, HTML

--- js/threesixty.js
+++ js/threesixty.js
@@ -1,30 +1,29 @@
 $(document).ready(function () {
-	var ready = false;
-	var dragging = false;
-	var pointerStartPosX = 0;
-	var pointerEndPosX = 0;
-	var pointerDistance = 0;
+	var ready = false,
+			dragging = false,
+			pointerStartPosX = 0,
+			pointerEndPosX = 0,
+			pointerDistance = 0,
 	
-	var monitorStartTime = 0;
-	var monitorInt = 10;
-	var ticker = 0;
+			monitorStartTime = 0,
+			monitorInt = 10,
+			ticker = 0,
+			speedMultiplier = 10,
+			spinner,
 	
-	var speedMultiplier = 10;
-	
-	var totalFrames = 180;
-	var currentFrame = 0;
-	var frames = [];
-	var endFrame = 0;
-	var loadedImages = 0;
-	
-	var spinner;
+			totalFrames = 180,
+			currentFrame = 0,
+			frames = [],
+			endFrame = 0,
+			loadedImages = 0;
 	
 	function addSpinner () {
 		spinner = new CanvasLoader("spinner");
 		spinner.setShape("spiral");
-		spinner.setDiameter(30);
+		spinner.setDiameter(90);
 		spinner.setDensity(90);
-		spinner.setSpeed(3);
+		spinner.setRange(1);
+		spinner.setSpeed(4);
 		spinner.setColor("#333333");
 		spinner.show();
 		$("#spinner").fadeIn("slow");
@@ -43,8 +42,9 @@ $(document).ready(function () {
 	
 	function imageLoaded() {
 		loadedImages++;
+		$("#spinner span").text(Math.floor(loadedImages / totalFrames * 100) + "%");
 		if (loadedImages == totalFrames) {
-			frames[0].removeClass("previous-image").addClass("current-image");;
+			frames[0].removeClass("previous-image").addClass("current-image");
 			$("#spinner").fadeOut("slow", function(){
 				spinner.hide();
 				showThreesixty();
@@ -56,34 +56,50 @@ $(document).ready(function () {
 	
 	function showThreesixty () {
 		$("#threesixty_images").fadeIn("slow");
-		
 		ready = true;
 		endFrame = -720;
 		refresh();
 	};
 	
 	addSpinner();
-	loadImage();	
+	loadImage();
+	
+	function render () {
+		if(currentFrame !== endFrame)
+		{	
+			var frameEasing = endFrame < currentFrame ? Math.floor((endFrame - currentFrame) * 0.1) : Math.ceil((endFrame - currentFrame) * 0.1);
+			hidePreviousFrame();
+			currentFrame += frameEasing;
+			showCurrentFrame();
+		} else {
+			window.clearInterval(ticker);
+			ticker = 0;
+		}
+	};
+	
+	function refresh () {
+		if (ticker === 0) {
+			ticker = self.setInterval(render, Math.round(1000 / 60));
+		}
+	};
+	
+	function hidePreviousFrame() {
+		frames[getNormalizedCurrentFrame()].removeClass("current-image").addClass("previous-image");
+	};
+	
+	function showCurrentFrame() {
+		frames[getNormalizedCurrentFrame()].removeClass("previous-image").addClass("current-image");
+	};
+	
+	function getNormalizedCurrentFrame() {
+		var c = -Math.ceil(currentFrame % totalFrames);
+		if (c < 0) c += (totalFrames - 1);
+		return c;
+	};
 	
 	function getPointerEvent(event) {
 		return event.originalEvent.targetTouches ? event.originalEvent.targetTouches[0] : event;
 	};
-	
-	$("#threesixty").live("touchstart", function (event) {
-		event.preventDefault();
-		pointerStartPosX = getPointerEvent(event).pageX;
-		dragging = true;
-	});
-	
-	$("#threesixty").live("touchmove", function (event) {
-		event.preventDefault();
-		trackPointer(event);
-	});
-	
-	$("#threesixty").live("touchend", function (event) {
-		event.preventDefault();
-		dragging = false;
-	});
 	
 	$("#threesixty").mousedown(function (event) {
 		event.preventDefault();
@@ -101,19 +117,21 @@ $(document).ready(function () {
 		trackPointer(event);
 	});
 	
-	function getNormalizedCurrentFrame() {
-		var c = -Math.ceil(currentFrame % totalFrames);
-		if (c < 0) c += (totalFrames - 1);
-		return c;
-	};
+	$("#threesixty").live("touchstart", function (event) {
+		event.preventDefault();
+		pointerStartPosX = getPointerEvent(event).pageX;
+		dragging = true;
+	});
 	
-	function hidePreviousImage() {
-		frames[getNormalizedCurrentFrame()].removeClass("current-image").addClass("previous-image");
-	};
+	$("#threesixty").live("touchmove", function (event) {
+		event.preventDefault();
+		trackPointer(event);
+	});
 	
-	function showCurrentImage() {
-		frames[getNormalizedCurrentFrame()].removeClass("previous-image").addClass("current-image");
-	};
+	$("#threesixty").live("touchend", function (event) {
+		event.preventDefault();
+		dragging = false;
+	});
 	
 	function trackPointer(event) {
 		if (ready && dragging) {
@@ -121,30 +139,10 @@ $(document).ready(function () {
 			if(monitorStartTime < new Date().getTime() - monitorInt) {
 				pointerDistance = pointerEndPosX - pointerStartPosX;
 				endFrame = currentFrame + Math.ceil((totalFrames - 1) * speedMultiplier * (pointerDistance / $("#threesixty").width()));
-				
 				refresh();
 				monitorStartTime = new Date().getTime();
 				pointerStartPosX = getPointerEvent(event).pageX;
 			}
-		}
-	};
-	
-	function refresh () {
-		if (ticker === 0) {
-			ticker = self.setInterval(function () { render(); }, Math.round(1000 / 60));
-		}
-	};
-	
-	function render () {
-		if(currentFrame !== endFrame)
-		{	
-			var inertia = endFrame < currentFrame ? Math.floor((endFrame - currentFrame) * 0.1) : Math.ceil((endFrame - currentFrame) * 0.1);
-			hidePreviousImage();
-			currentFrame += inertia;
-			showCurrentImage();
-		} else {
-			window.clearInterval(ticker);
-			ticker = 0;
 		}
 	};
 });
